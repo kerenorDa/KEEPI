1ecc7e25e72b6b9fce546fbc63f9a1f9197739ed
Keren Or <kerenco@ac.sce.ac.il>
2024-11-21 22:58:40 +0200

    chat feature

---
+++ Client/Repositories/Interfaces/IChatRepository.cs
@@ -0,0 +1,13 @@
+﻿using Keepi.Shared;
+
+namespace Keepi.Client.Repositories.Interfaces
+{
+    public interface IChatRepository
+    {
+        Task<List<Chat>> CreateNewChatFile(string user1, string user2);
+        Task<List<bool>> AddMessageToChat(string fileName, string userId, string message);
+        Task<List<ChatData>> ReadChatFile(string fileName, string userId);
+        Task<List<Chat>> GetUserChats(string user);
+        Task<List<bool>> DeleteChat(string fileName);
+    }
+}
---
+++ Server/Controllers/NotificationsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Keepi.Shared;
+
+namespace Keepi.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly Db_Context _context;
+
+        public NotificationsController(Db_Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("readFile/{userId}")]
+        public async Task<List<NotificationsData>> ReadFile(string userId)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var data = await NotificationsHelper.ReadFile(userId);
+                    return new List<NotificationsData> { data };
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+            return null;
+        }
+
+        [HttpGet("newNotifications/{userId}")]
+        public async Task<List<bool>> CheckForNewNotifications(string userId)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var b = await NotificationsHelper.CheckForNewNotifications(userId);
+                    return new List<bool> { b };
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
