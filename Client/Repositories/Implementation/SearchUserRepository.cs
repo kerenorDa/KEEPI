e563aae406cb2930e8a3c2351a3351c751b217b3
Keren Or <kerenco@ac.sce.ac.il>
2024-11-21 22:57:39 +0200

    search user feature

---
+++ Client/Repositories/Implementation/SearchUserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Keepi.Client.Communication;
+using Keepi.Client.Repositories.Base;
+using Keepi.Client.Repositories.Interfaces;
+using Keepi.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using static Keepi.Client.Pages.RegisterPage;
+
+namespace Keepi.Client.Repositories.Implementation
+{
+    public class SearchUserRepository : Repository, ISearchUserRepository
+    {
+        private const string URL = "api/SearchUser";
+
+        public SearchUserRepository(IHttpService commService) : base(commService)
+        {
+            
+        }
+
+        //public async Task<object> Register(RegistrationModel user) =>
+        //     await Post<RegistrationModel>(URL + System.IO.Path.AltDirectorySeparatorChar + "register", user);
+
+        //public async Task<List<User>> Register(RegistrationModel user) =>
+        //   await Get<User>($"api/Register/_register/{user}");
+
+        public async Task<List<User>> GetUserByUserName(string userName) =>
+           await Get<User>($"api/SearchUser/getUserByUserName/{userName}");
+        
+        public async Task<List<string>> SearchUsers(string prefix) =>
+           await Get<string>($"api/SearchUser/searchUsers/{prefix}");
+    }
+
+}
---
+++ Server/Controllers/SearchUserController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using Keepi.Shared;
+using static Duende.IdentityServer.Models.IdentityResources;
+
+namespace Keepi.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SearchUserController : ControllerBase
+    {
+        private readonly Db_Context _context;
+
+        public SearchUserController(Db_Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getUserByUserName/{userName}")]
+        public async Task<List<User>> GetUserByUserName(string userName)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    User user = await _context.Users.FirstOrDefaultAsync(u => u.Username == userName);
+                    if (user == null)
+                    {
+                        return new List<User> { };
+                    }
+                    return new List<User> { user };
+                }
+            }
+            catch (Exception)
+            {
+                return new List<User> { };
+            }
+
+            return new List<User> { };
+        }
+
+        [HttpGet("searchUsers/{prefix}")]
+        public async Task<List<string>> SearchUsers(string prefix)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var users = await _context.Users.Where(u => u.Username.StartsWith(prefix)).Select(u => u.Username).ToListAsync();
+                    
+                    return users;
+                }
+            }
+            catch (Exception)
+            {
+                return new List<string> { };
+            }
+
+            return new List<string> { };
+        }
+    }
+
+}
